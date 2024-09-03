f0e08fb52de91b4e5542276123a0e03eb6884304
titus fx <titusfx@gmail.com>
2024-09-03 10:35:50 +0200

    feat(client): add client to do api calls

---
+++ client_app.py
@@ -0,0 +1,24 @@
+from gradio_client import Client
+
+# Initialize the client with the URL of the Gradio service
+client = Client("http://127.0.0.1:7860/")
+
+# Uncomment the next line to see api doc
+#client.view_api() # Shows the api info
+# Client.predict() Usage Info
+# ---------------------------
+# Named API endpoints: 1
+
+#  - predict(name, api_name="/greet") -> value_4
+#     Parameters:
+#      - [Textbox] name: str (required)  
+#     Returns:
+#      - [Textbox] value_4: str 
+
+# Call the Gradio interface with the input data
+result = client.predict(name="Alice",api_name="/greet")  # Replace "Alice" with the input you want to test
+
+# Print the result returned by the Gradio service
+print(result)
+
+
