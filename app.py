a401e6081239e566c510cb3ce863622f0d5e3b53
titus fx <titusfx@gmail.com>
2024-09-03 10:35:30 +0200

    feat(endpoint): add name to endpoint

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ def greet(name):
     return f"Hello, {name}!"
 
 # Create the Gradio interface
-interface = gr.Interface(fn=greet, inputs="text", outputs="text")
+# interface = gr.Interface(fn=greet, inputs="text", outputs="text").api(name="/greet")
+# btn = gr.Button("Execute")
+# btn.click(fn=greet, inputs=inp, outputs=out)
 
+with gr.Blocks() as app:
+    gr.Markdown("Start typing below and then click **Run** to see the output.")
+    with gr.Row():
+        inp = gr.Textbox(placeholder="What is your name?")
+        out = gr.Textbox()
+    btn = gr.Button("Run")
+    btn.click(fn=greet, inputs=[inp], outputs=[out])
+
+    
 # Launch the Gradio app
 if __name__ == "__main__":
-    interface.launch(server_name="0.0.0.0", server_port=7860)
+    app.launch(server_name="0.0.0.0", server_port=7860)
 
